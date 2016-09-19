c548bc7be498b076185aff9f7b9f0267b9ac59c6
James Ruchala <jruchala@gmail.com>
2016-09-18 21:54:36 -0400

    add mightygumball.js to stage

--- mightygumball.js
+++ mightygumball.js
@@ -1,9 +1,13 @@
-var url = "http://someserver.com/data.json";
-var request = new XMLHttpRequest();
-request.open("GET", url);
+
+
 request.onload = function(){
-  if (request.status==200){
-    alert("Data received!");
+  var url = "http://localhost/sales.json";
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = function(){
+    if (request.status == 200){
+      updateSales(request.responseText);
+    }
   }
 };
 request.send(null);
