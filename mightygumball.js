89dc46cfc4be066f7d745da931c971beed76dda6
James Ruchala <jruchala@gmail.com>
2016-09-27 20:20:53 -0400

    add javascript funcitonality to update stream in real time

--- mightygumball.js
+++ mightygumball.js
@@ -1,18 +1,50 @@
+/* mightygumball.js */
+/*
+ * get the content of a JSON file using Ajax 
+ *
+ */
 
+window.onload = init;
+var lastReportTime = 0;
+function init() {
+  setInterval(handleRefresh, 3000);
 
-request.onload = function(){
-  var url = "http://localhost/gumball/sales.json";
-  var request = new XMLHttpRequest();
-  request.open("GET", url);
-  request.onload = function(){
-    if (request.status == 200){
-      updateSales(request.responseText);
-    }
+//  getSales();
+}
+
+function getSales() {
+}
+
+function handleRefresh(){
+  var url = "http://gumball.wickedlysmart.com" +
+    "?callback=updateSales" + 
+    "&lastreporttime=" + lastReportTime +
+    "&random=" + (new Date()).getTime();
+
+  var newScriptElement = document.createElement("script");
+  newScriptElement.setAttribute("src", url);
+  newScriptElement.setAttribute("id", "jsonp");
+
+  var oldScriptElement = document.getElementById("jsonp");
+  var head = document.getElementsByTagName("head")[0];
+  if(oldScriptElement == null){
+    head.appendChild(newScriptElement);
   }
-};
-request.send(null);
+  else{
+    head.replaceChild(newScriptElement, oldScriptElement);
+  }
+}
 
-function updateSales(responseText){
+function updateSales(sales) {
   var salesDiv = document.getElementById("sales");
-  salesDiv.innerHTML = responseText;
+   for (var i = 0; i < sales.length; i++) {
+    var sale = sales[i];
+    var div = document.createElement("div");
+    div.setAttribute("class", "saleItem");
+    div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
+    salesDiv.appendChild(div);
+  }
+  if(sales.length > 0){
+    lastReportTime = sales[sales.length-1].time;
+  }
 }
